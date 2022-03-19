8d48c4b69609357fd163c0e31e78afeb5ce5782d
Md-Jaabir <100750999+Md-Jaabir@users.noreply.github.com>
2022-03-19 11:13:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ const express=require("express");
 const app=express();
 const mongoose =require("mongoose");
 const dotenv=require("dotenv");
-const cors=require('cors');
 
 dotenv.config();
 app.use(express.json());
-app.use(cors({
-   origin:"*"
-}));
+
 
 app.get("/",(req,res)=>{
 	res.json({"Message":"Hello"})
