f94c186239d60eb5c65b759f37f41075f4f26327
MD Jaabir <ibnchowdhury345@gmail.com>
2022-03-20 16:02:40 +0600

    hello

--- index.js
+++ index.js
@@ -41,9 +41,6 @@ app.get("/",(req,res)=>{
 app.post("/send",(req,res)=>{
 	let newMsg=new msg(req.body);
 	newMsg.save()
-	.then(()=>{
-		res.json({"Message":"success"})
-	})
 	// .catch((err)=>{
 	// 	res.json({"Message":"error"})
 	// });
