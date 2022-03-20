b9f05961e8518661ec9429b975109a131f1b2e68
MD Jaabir <ibnchowdhury345@gmail.com>
2022-03-20 15:12:49 +0600

    connecting with database

--- index.js
+++ index.js
@@ -9,21 +9,46 @@ app.use(cors({origin:"*"}));
 dotenv.config();
 app.use(express.json());
 
+mongoose.connect(process.env.DB,{
+	useUnifiedTopology:true,
+	useNewUrlParser:true
+}).then(()=>{
+	console.log("connected successfully")
+});
+
+const roomSchema = mongoose.Schema({
+	roomname:String,
+	password:String,
+	authorname:String
+});
+
+const room=mongoose.model("room",roomSchema);
+
+const chatSchema = mongoose.Schema({
+	roomname:String,
+	password:String,
+	name:String,
+	message:String
+});
+
+const msg=mongoose.model("msg",chatSchema);
+
 
 app.get("/",(req,res)=>{
 	res.json({"Message":"Hello"})
 })
 
 app.post("/send",(req,res)=>{
-	res.json(req.body);
+	
 })
 
 app.get("/fetch",(req,res)=>{
 	res.json(req.query);
 })
 
-app.post("/create_room",(req,res)=>{
-	res.json(req.body);
+app.post("/create_room",async(req,res)=>{
+	let existedRoom=await room.find({roomname:req.body.roomname});
+	console.log(existedRoom)
 })
 
 app.listen(process.env.PORT);
